ee985cc39707fcc1f9a43944e410002654f191fe
ina-uzu <ina.uzu.song@gmail.com>
2018-08-10 17:16:26 +0900

    first commit

---
+++ app/src/main/java/com/example/ina97/arii/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ina97.arii;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Main);
+
+    }
+}
