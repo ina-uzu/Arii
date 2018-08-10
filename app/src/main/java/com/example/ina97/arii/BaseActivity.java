77a3e3c463e5e3c44c2242b354bd803cea5c1aa8
ina-uzu <ina.uzu.song@gmail.com>
2018-08-11 02:56:20 +0900

    RecyclerVIew practice!

---
+++ app/src/main/java/com/example/ina97/arii/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.example.ina97.arii;
+
+import android.app.Activity;
+
+public class BaseActivity extends Activity {
+
+
+}
---
+++ app/src/main/java/com/example/ina97/arii/Item_club_news.java
@@ -0,0 +1,29 @@
+package com.example.ina97.arii;
+
+import android.widget.ImageView;
+
+public class Item_club_news {
+    String title;
+    String cont;
+
+    public Item_club_news(String title, String cont){
+        this.cont=cont;
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getCont(){
+        return cont;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public void setCont(String cont){
+        this.cont=cont;
+    }
+}
